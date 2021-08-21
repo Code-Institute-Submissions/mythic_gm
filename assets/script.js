106c06d19195d5a247808cb0a6550fbf89a70982
Daniel Callaghan <dan_cal89@hotmail.com>
2021-08-21 12:05:11 +0000

    add google font, add how to modal dialouge box with placeholder text

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// get how to section modal elements
+let HowToModal = document.getElementById('how-to-modal');
+let howToBtn = document.getElementById('how-to-btn');
+let closeHowTo = document.getElementsByClassName('close')[0];
+
+//open and close how to modal
+howToBtn.onclick = function() {
+  HowToModal.style.display = "initial";
+}
+
+closeHowTo.onclick = function() {
+  HowToModal.style.display = "none"
+}
