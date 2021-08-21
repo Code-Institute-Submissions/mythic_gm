30a856cf694fdd74eb8ced87c7bf80f02d2f2cd4
Daniel Callaghan <dan_cal89@hotmail.com>
2021-08-21 18:21:38 +0000

    add script to open and close tabs in emulator window

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,44 @@ window.onclick = function(event) {
     HowToModal.style.display = "none";
   }
 }
+
+// Open and close tab divs
+
+// declare variables - tab buttons and content of the tabs
+let activeTab;
+let tablinks = document.getElementsByClassName('tabcontent');
+let oracleButton = document.getElementById('oracle-button')
+let oracleContent = document.getElementById('oracle');
+let sceneButton = document.getElementById('scene-button')
+let sceneContent = document.getElementById('scene');
+let listButton = document.getElementById('list-button')
+let listsContent = document.getElementById('list')
+
+//set initial view - oracle tab open and the other tabs closed
+listsContent.style.display = "none";
+sceneContent.style.display = "none";
+
+//close tabs
+function closeTabs() {
+  for(let i = 0; i < tablinks.length; i++){
+    tablinks[i].style.display = "none";
+  }
+}
+
+//open tabs
+oracleButton.onclick = function() { 
+  closeTabs();
+  oracleContent.style.display = "block"
+}
+
+sceneButton.onclick = function() { 
+  closeTabs();
+  sceneContent.style.display = "block"
+}
+
+listButton.onclick = function() { 
+  closeTabs();
+  listsContent.style.display = "block"
+}
+
+
